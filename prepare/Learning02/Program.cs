df67cc57488520336dd0ea6d294cc0b50ae5b206
profepabloheredia <profepabloheredia@gmail.com>
2024-05-02 20:47:14 -0300

    changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1= new Job();
+        job1._jobTitle= "Software Engineer";
+        job1._company="Microsoft";
+        job1._startYear=2019;
+        job1._endYear=2022;
+        
+
+        Job job2= new Job();
+        job2._jobTitle= "Software Engineer";
+        job2._company="Applee";
+        job2._startYear=2022;
+        job2._endYear=2024;
+        
+
+        Resume myResume= new Resume();
+        myResume._name="Pablo Heredia";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
+
     }
 }
